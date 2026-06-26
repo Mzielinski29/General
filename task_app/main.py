8b87ec3e1e1f85c1ef4c426378ab953b7ccf8d68
Maciej191 <maciej.zielinski191@interia.pl>
2026-06-26 11:49:45 +0200

    added show_all_tasks, improved flags and other small things

--- task_app/main.py
+++ task_app/main.py
@@ -1,6 +1,7 @@
 import time
 import argparse
 import json
+import sys
 from pathlib import Path
 import os
 from enum import Enum
@@ -43,7 +44,7 @@ class Task:
         })
 
     def show_task(self):
-        return f'{self.name} - created: {self.date}\n{self.status.value} - last modified: {time.ctime()}\n{self.content}'
+        print(f'{self.name} - created: {self.date}\n{self.status.value} - last modified: {time.ctime()}\n{self.content}')
 
     def edit_task_title(self, new_title):
         self.name = new_title
@@ -51,7 +52,8 @@ class Task:
         save_task()
 
     def edit_task_content(self, new_content):
-        user_action = ask_user()
+        msg = f'Are you sure you want to overwrite the content?\nWARNING! Current content will be lost [Yes/No/Cancel]: '
+        user_action = ask_user(msg)
 
         while user_action != UserActions.CANCEL:
             if user_action == UserActions.YES:
@@ -62,7 +64,7 @@ class Task:
             elif user_action == UserActions.NO:
                 new_content = input('Enter the new content again: ')
 
-            user_action = ask_user()
+            user_action = ask_user(msg)
 
     def change_task_status(self, new_status):
         self.status = new_status
@@ -78,10 +80,12 @@ class Task:
 
 #----------------MAIN BODY----------------
 
-TASKS = {}
-NEXT_ID = 1
 BASE_DIR = Path(__file__).resolve().parent
 DATA_FILE = BASE_DIR / "tasks.json"
+COMMANDS = ['add-task','show-task','show-all-tasks','edit-title','edit-content','change-status','task-history']
+
+
+#--------------CONNECT JSON---------------
 
 def load_tasks():
     global TASKS, NEXT_ID
@@ -140,16 +144,56 @@ def save_task():
 def get_task(task_id):
     return TASKS.get(task_id)
 
-def ask_user():
+def show_all_tasks():
+
+    if not TASKS:
+        print("No tasks found.")
+        return
+
+    print("ID | Title | Status")
+    print("-------------------")
+
+    for task_id, task in TASKS.items():
+        print(f'{task_id} | {task.name} | {task.status.value}')
+
+def ask_user(msg):
     while True:
-        #input for content change only, subject to change to var later
-        answer = input(f'Are you sure you want to overwrite the content?\nWARNING! Current content will be lost [Yes/No/Cancel]: ').strip().lower()
+        answer = input(msg).strip().lower()
 
         try:
             return UserActions(answer)
 
         except ValueError:
-            print("Invalid option.")
+            print('Invalid option.')
+
+def require_args(command, *required):
+    missing = []
+
+    for arg in required:
+        if getattr(args, arg) is None:
+            missing.append(f'--{arg}')
+
+    if missing:
+        print(
+            f'Command "{command}" requires: '
+            + ", ".join(missing)
+        )
+        sys.exit()
+
+def forbid_args(command, *forbidden):
+
+    invalid = []
+
+    for arg in forbidden:
+        if getattr(args, arg) is not None:
+            invalid.append(f'--{arg}')
+
+    if invalid:
+        print(
+            f'Command "{command}" does not use: '
+            + ", ".join(invalid)
+        )
+        sys.exit()
 
 
 if __name__ == "__main__":
@@ -157,21 +201,65 @@ if __name__ == "__main__":
     load_tasks()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("command")
+    parser.add_argument("command", nargs='?')
     parser.add_argument("--id", type=int)
     parser.add_argument("--title")
     parser.add_argument("--content")
+    parser.add_argument("--status")
+    parser.add_argument("--commands",
+                        help='show list of commands',
+                        action='store_true')
+    parser.add_argument("--usages",
+                        help='examples of use',
+                        action='store_true')
 
     args = parser.parse_args()
 
-    if args.command == "add":
+    if args.commands:
+        print(f'list of commands:')
+        for command in COMMANDS:
+            print(f'\t{command}')
+        sys.exit()
+    elif args.usages:
+        print(f'usage examples:\n\t[this_file] add-task --title "[title]" --content "[content]"\n\t[this_file] edit-title --title "[new_title]"')
+        sys.exit()
+
+    if args.command == 'add-task':
+        require_args('add-task', 'title', 'content')
+        forbid_args('id', 'status')
         task_id = create_task(args.title, args.content)
         print(f"Task created with ID {task_id}")
-    elif args.command == "show":
+    elif args.command == 'show-task':
+        require_args('show-task', 'id')
         task = get_task(args.id)
         if task:
             task.show_task()
-    elif args.command == "edit-title":
+    elif args.command == 'show-all-tasks':
+        show_all_tasks()
+    elif args.command == 'edit-title':
+        require_args('edit-title', 'id', 'title')
+        forbid_args('content', 'status')
         task = get_task(args.id)
         if task:
             task.edit_task_title(args.title)
+    elif args.command == 'edit-content':
+        require_args('edit-content', 'id', 'content')
+        forbid_args('title', 'status')
+        task = get_task(args.id)
+        if task:
+            task.edit_task_content()
+    elif args.command == 'change-status':
+        require_args('change-status', 'id', 'status')
+        forbid_args('title', 'content')
+        task = get_task(args.id)
+        if task:
+            task.change_task_status()
+    elif args.command == 'task-history':
+        require_args('task-history', 'id')
+        forbid_args('title', 'content', 'status')
+        task = get_task(args.id)
+        if task:
+            task.view_task_history()
+    else:
+        print(f'Invalid syntax\t try --help for usage or commands for list of commands')
+
